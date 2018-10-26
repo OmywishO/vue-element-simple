24f645933c57d3ccf76bfde4457eb09cee9598f4
wish <weishiyouxiang@126.com>
2018-10-26 17:37:29 +0800

    初始化项目

---
+++ src/views/errorPage/401.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <el-button type="primary">401</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error404'
+}
+</script>
---
+++ src/views/errorPage/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <el-button type="primary">404</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error404'
+}
+</script>
